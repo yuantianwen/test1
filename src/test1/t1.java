1a06d86a965ddc75e9e77cd8e4ceeb5e64278ad2
Administrator <Administrator@AN4WFEN0KENJY3J>
2018-01-29 06:21:45 +0800

    aa

---
+++ src/test1/t1.java
@@ -0,0 +1,13 @@
+package test1;
+
+public class t1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("class t1");
+		System.out.println("class t2");
+		System.out.println("class t3");
+		System.out.println("class t4");
+	}
+
+}
